75e593ddf0f3efa7fa9dd3c4cd63c95ccc71eb3d
Ryan Boldi <Ryan.boldi123@gmail.com>
2018-09-19 16:42:40 +0400

    Initial Program skeleton

---
+++ EstimatePi.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 19 16:34:58 2018
+
+@author: ryan-pc
+
+Created By: Ryan Boldi
+"""
+import math
+
+PI = math.pi
+SQUARESIDE = 1
+NEEDLES = 1000
+
+
+
+
+
+#Assuming we can figure out the circumference of a circle.
