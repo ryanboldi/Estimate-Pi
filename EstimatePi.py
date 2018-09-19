cffb40d13d608e0fa95726afa5d9e135fd8f1507
Ryan Boldi <Ryan.boldi123@gmail.com>
2018-09-19 19:56:42 +0400

    Program now outputs a graph

--- EstimatePi.py
+++ EstimatePi.py
@@ -7,13 +7,38 @@ Created on Wed Sep 19 16:34:58 2018
 Created By: Ryan Boldi
 """
 import math
+import matplotlib.pyplot as plt
+import random
 
 PI = math.pi
-SQUARESIDE = 1
-NEEDLES = 1000
+SQUARESIZE = 2
+RADIUS = SQUARESIZE/2
+SQUAREA = SQUARESIZE*SQUARESIZE
+NEEDLESMAX = 100000
+
+circleNeedles = 0
+
+
+needleAmount = []
+errorAmount = []
+
+
+for i in range(0, NEEDLESMAX):
+    #random.random returns 0 <= x < 1
+    x = (random.random()*SQUARESIZE)-RADIUS #-1 <= x < 1
+    y = (random.random()*SQUARESIZE)-RADIUS
+    
+    if (x**2 + y**2 < 1):   #1 for radius of 1 (root(1))
+        circleNeedles += 1
+        
+    pi = (circleNeedles/NEEDLESMAX)*SQUAREA
+    error = math.fabs(PI-pi)
+    
+    
+    needleAmount.append(i)
+    errorAmount.append(error)
 
 
 
-
-
-#Assuming we can figure out the circumference of a circle.
+plt.plot(needleAmount, errorAmount, ls = 'steps')
+plt.show()
