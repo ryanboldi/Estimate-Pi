455988edc9aaaa57052338bfb4dcf9b4f0013d8f
Ryan Boldi <Ryan.boldi123@gmail.com>
2018-12-19 12:51:32 +0400

    Added feature to average over many simulations

--- EstimatePi.py
+++ EstimatePi.py
@@ -1,13 +1,7 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Sep 19 16:34:58 2018
-
-@author: ryan-pc
-
-Created By: Ryan Boldi
-"""
 import math
 import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('seaborn-ticks')
 import random
 import numpy as np
 
@@ -15,11 +9,7 @@ PI = math.pi
 SQUARESIZE = 2
 RADIUS = SQUARESIZE/2
 SQUAREA = SQUARESIZE*SQUARESIZE
-NEEDLESMAX = 10000
-
-
-
-
+NEEDLESMAX = 100000
 
 def randomThrows(SQUARESIZE, RADIUS, SQUAREA, NEEDLESMAX):
     
@@ -29,8 +19,7 @@ def randomThrows(SQUARESIZE, RADIUS, SQUAREA, NEEDLESMAX):
     errorAmount = []
     piValues = []
     
-    
-    for i in range(0, NEEDLESMAX):
+    for i in range(1, NEEDLESMAX+1):
         #random.random returns 0 <= x < 1
         x = (random.random()*SQUARESIZE)-RADIUS #-1 <= x < 1
         y = (random.random()*SQUARESIZE)-RADIUS
@@ -38,9 +27,8 @@ def randomThrows(SQUARESIZE, RADIUS, SQUAREA, NEEDLESMAX):
         if (x**2 + y**2 < 1):   #1 for radius of 1 (root(1))
             circleNeedles += 1
             
-        pi = (circleNeedles/NEEDLESMAX)*SQUAREA
+        pi = (circleNeedles/i)*SQUAREA
         error = math.fabs(PI-pi)
-    
     
         needleAmount.append(i)
         errorAmount.append(error)
@@ -48,8 +36,7 @@ def randomThrows(SQUARESIZE, RADIUS, SQUAREA, NEEDLESMAX):
     
     return needleAmount,errorAmount,piValues
 
-
-
+#maybe if there was a mesh of needles dropped with a linear spacing, it would increase the accuracy
 def meshThrows(SQUARESIZE, RADIUS, SQUAREA, NEEDLESMAX):
     circleNeedles = 0
     
@@ -57,36 +44,39 @@ def meshThrows(SQUARESIZE, RADIUS, SQUAREA, NEEDLESMAX):
     errorAmount = []
     piValues = []
     
-    needlesPerRow = np.sqrt(NEEDLESMAX)
-    dif = SQUARESIZE/needlesPerRow
+    line = int(np.floor(np.sqrt(NEEDLESMAX)))
+    print(line)
+    vals = np.linspace(0,2,line)
     
-    x = 0
-    y = 0
-    for i in range (0, NEEDLESMAX):
+    for i in range(1,len(vals)):
+        for j in range(1, len(vals)):
+            x = vals[i]
+            y = vals[j]
             
-            x+=dif
-            y+=dif
-        
             if (x**2 + y**2 < 1):   #1 for radius of 1 (root(1))
                 circleNeedles += 1
-                
-            pi = (circleNeedles/NEEDLESMAX)*SQUAREA
+            
+            pi = (circleNeedles/((i*line)+j))*SQUAREA
             error = math.fabs(PI-pi)
-    
-    
-            needleAmount.append(i)
+
+            needleAmount.append((i*line)+j)
             errorAmount.append(error)
             piValues.append(pi)
     
-    return needleAmount,errorAmount,piValues
+    return needleAmount, errorAmount, piValues
 
-#randomNeedle, randomError, randomPi = randomThrows(SQUARESIZE, RADIUS, SQUAREA, NEEDLESMAX)
-meshNeedle, meshError, meshPi = meshThrows(SQUARESIZE, RADIUS, SQUAREA, NEEDLESMAX)
+pivalue = 0
+num = 100
+for i in range (0,num):
+        randomNeedle, randomError, randomPi = randomThrows(SQUARESIZE, RADIUS, SQUAREA, NEEDLESMAX)
+        pivalue += randomPi[-1]
+acPi = pivalue/num
+#average pi values over 1000 simulations
 
-plt.plot(meshNeedle, meshError, ls = 'steps')
+plt.plot(randomNeedle, randomError, ls = 'steps')
 plt.grid()
-plt.xlabel("Number of needles Dropped")
+plt.xlabel("Number of needles dropped")
 plt.ylabel("Error")
 plt.show()
 
-print("Best Value of pi ",  meshPi[-1])
+print("Best Value of pi ",  acPi)
